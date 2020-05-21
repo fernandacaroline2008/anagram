f84cf5738445a1bf7e39d589e74a5cd33e2fde56
fernandacaroline2008 <fernandacaroline2008@gmail.com>
2020-05-20 18:06:26 -0600

    Add isAnagrams function and unit tests

---
+++ src/main/java/WordFunction.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+public class WordFunction {
+
+    public static boolean isAnagrams(String word1, String word2) {
+        word1 = format(word1);
+        word2 = format(word2);
+
+        if (word1.length() != word2.length()) {
+            return false;
+        }
+
+        if (word1.equals(word2)) {
+            return true;
+        }
+
+        String word1Sorted = sort(word1);
+        String word2Sorted = sort(word2);
+
+        return word1Sorted.equals(word2Sorted);
+    }
+
+    private static String format(String word) {
+        return word.replace(" ", "").toLowerCase();
+    }
+
+    private static String sort(String word) {
+        char[] chars = word.toCharArray();
+        Arrays.sort(chars);
+        return new String(chars);
+    }
+}
---
+++ src/test/java/WordFunctionTest.java
@@ -0,0 +1,44 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class WordFunctionTest {
+
+    @Test
+    public void isAnagrams_whenWordsAreEquals_shouldReturnTrue() {
+        String word1 = "cinema";
+        String word2 = "cinema";
+        assertTrue(WordFunction.isAnagrams(word1, word2));
+    }
+
+    @Test
+    public void isAnagrams_whenWordsAreAnagrams_shouldReturnTrue() {
+        String word1, word2;
+
+        word1 = "cinema";
+        word2 = "iceman";
+        assertTrue(WordFunction.isAnagrams(word1, word2));
+
+        word1 = "Listen";
+        word2 = "Silent";
+        assertTrue(WordFunction.isAnagrams(word1, word2));
+
+        word1 = "Funeral";
+        word2 = "Real fun";
+        assertTrue(WordFunction.isAnagrams(word1, word2));
+    }
+
+    @Test
+    public void isAnagrams_whenWordsHaveDifferentLength_shouldReturnFalse() {
+        String word1 = "save";
+        String word2 = "car";
+        assertFalse(WordFunction.isAnagrams(word1, word2));
+    }
+
+    @Test
+    public void isAnagrams_whenWordsAreNotAnagrams_shouldReturnFalse() {
+        String word1 = "car";
+        String word2 = "art";
+        assertFalse(WordFunction.isAnagrams(word1, word2));
+    }
+}
